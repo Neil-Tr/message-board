c7d7bf966112bce8668d20c5b01879b8eea7e5ab
neilt <neiltran.syd@gmail.com>
2026-03-10 21:50:51 +1100

    finish

--- app.js
+++ app.js
@@ -2,16 +2,42 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Mang chang",
+    added: new Date(),
+  },
+  {
+    text: "Hi-fi",
+    user: "Ming ching",
+    added: new Date(),
+  },
+];
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "views")));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
+  res.render("index", { title: "Home", messages });
 });
 
 app.get("/new", (req, res) => {
   res.render("new", { title: "New Message" });
+});
+
+app.post("/new", (req, res) => {
+  const { author, message } = req.body;
+  if (author && message) {
+    messages.unshift({
+      text: message,
+      user: author,
+      added: new Date(),
+    });
+  }
+  res.redirect("/");
 });
 
 app.use((req, res) => {
