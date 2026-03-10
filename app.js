0d9c737b914a90b3dfc5df0d4075c544ca9c8996
neilt <neiltran.syd@gmail.com>
2026-03-10 21:31:05 +1100

    Add dynamic titles and styles

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "views")));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+app.get("/new", (req, res) => {
+  res.render("new", { title: "New Message" });
+});
+
+app.use((req, res) => {
+  res.status(404).render("404", { title: "Not Found" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
